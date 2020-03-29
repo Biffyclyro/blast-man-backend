bff3cc3bbc0c82c91a1be37a74cef1a7c3256dc4
Biffyclyro <joao-senna1@hotmail.com>
2020-03-29 20:16:12 -0300

    websocket pegando

--- src/main/java/com/fliperama/blastmanbackend/controllers/GameController.java
+++ src/main/java/com/fliperama/blastmanbackend/controllers/GameController.java
@@ -1,5 +1,8 @@
 package com.fliperama.blastmanbackend.controllers;
 
+import com.fliperama.blastmanbackend.entity.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class GameController {
 
+    private Player player1;
+    private boolean logado = false;
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @PostMapping("/connect")
+    public void connectPlayer(@RequestBody Player player){
+        this.player1 = player;
+        this.logado = true;
+        template.convertAndSend("/topic/notification", true);
+    }
+
+
+
     @PostMapping("/move")
-    public void movePlayer(){
-        System.out.println("foi");
+    public void movePlayer(@RequestBody Player player){
+        this.player1 = player;
+       /* template.convertAndSend("/topic/notification", player);*/
     }
 }
---
+++ src/main/java/com/fliperama/blastmanbackend/entity/Player.java
@@ -0,0 +1,10 @@
+package com.fliperama.blastmanbackend.entity;
+
+import lombok.Data;
+
+@Data
+public class Player {
+    private boolean alive;
+    private double x;
+    private double y;
+}
